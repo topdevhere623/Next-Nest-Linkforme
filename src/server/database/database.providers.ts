02deadba9370ddc2a869572338f5656f8fef6539
Piotr Blachnio <piotr.blachnio@yahoo.com>
2022-02-11 14:26:59 +0100

    2

--- src/server/database/database.providers.ts
+++ src/server/database/database.providers.ts
@@ -4,6 +4,6 @@ export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
     useFactory: (): Promise<typeof mongoose> =>
-      mongoose.connect(process.env.NODE_ENV === 'production' ? 'mongodb://linkforme:asf23gsgwaeagfsaf@mongo:27017/' : 'mongodb://localhost:27017/linkforme'),
+      mongoose.connect(process.env.NODE_ENV === 'production' ? 'mongodb+srv://admin:admin@cluster0.ntbit.mongodb.net/myFirstDatabase?retryWrites=true&w=majority' : 'mongodb://localhost:27017/linkforme'),
   },
 ];
